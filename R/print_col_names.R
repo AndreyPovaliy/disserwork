890ea33196ca9f7641773d61d522c29c52dd06d7
AndreyPovaliy <povandrey16@yandex.ru>
2025-11-05 16:10:37 +0300

    update data

---
+++ R/print_col_names.R
@@ -0,0 +1,20 @@
+#' Print colnames
+#'
+#' @param df Data frame from which needs colnames
+#'
+#'
+#'
+#' @return formated string in terminal
+#' @export
+#'
+#' @examples
+#' df <- mtcars
+#' print_col_names(df)
+
+
+
+print_col_names <- function(df){
+  cat(paste0("\"", colnames(df), "\"",
+             collapse = ", "))
+
+}
