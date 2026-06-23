a013a59303f374812ae0fd77465dcc619fb23710
Andrey Povaliy <povandrey16@yandex.ru>
2026-06-23 16:13:28 +0300

    add find_key

---
+++ R/find_key.R
@@ -0,0 +1,37 @@
+#' Rewrite bib keys
+#'
+#' @param link_file Links from file
+#' @param bib_file Actual bib File
+#'
+#'
+#' @return String for table
+#' @export
+
+#' @import tidytext
+#' @import stringr
+#' @import tibble
+#' @import dplyr
+#' @import readxl
+#' @importFrom bib2df bib2df
+#' 
+#'  @examples
+#' # links <- "links.xlsx"
+#' # bib_lnks <- "references.bib"
+
+
+
+find_key <- function(link_file, bib_file, n){
+  links <- read_excel(link_file)
+  bib_lnks <- bib2df(bib_file)
+  
+  for (i in 1:nrow(bib_lnks)){
+    if(
+      grepl(str_sub(bib_lnks$TITLE[i], 1, 30), links$link[links$n == n], 
+            fixed = TRUE)
+    ){
+      print(bib_lnks$BIBTEXKEY[i])
+      print(links$link[links$n == n])
+    }
+  }
+  
+}
